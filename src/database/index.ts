e2b9817a338342514454545c38fab3f7e4fa9cf9
Nathally <nathallyccd@gmail.com>
2022-10-04 16:43:19 -0300

    db

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,7 @@ import { User } from "../entities/User"
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
-    database: "./src/database/db.sqlite",
+    database: "../../database/db.sqlite",
     entities: [
         User
     ],
